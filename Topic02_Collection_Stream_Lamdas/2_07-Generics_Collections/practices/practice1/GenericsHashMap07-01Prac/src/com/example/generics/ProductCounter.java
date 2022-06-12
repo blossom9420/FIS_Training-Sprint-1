4bc3c0c3f9d2f0929ebbd0729af1a63d38b1fe57
Hoang Long <hoanglong.k18cntt@gmail.com>
2022-06-13 04:19:57 +0700

    1_14-Exceptions_D83527GC10_ag - done

---
+++ Topic02_Collection_Stream_Lamdas/2_07-Generics_Collections/practices/practice1/GenericsHashMap07-01Prac/src/com/example/generics/ProductCounter.java
@@ -0,0 +1,40 @@
+package com.example.generics;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ProductCounter {
+    // Create a Counting Map
+    private Map<String, Long> productCountMap = new HashMap<>();
+    // Create a Name Mapping Map
+    private Map<String, String> productNames = new TreeMap<>();
+
+    public static void main(String[] args) {
+
+        // List of part data
+        String[] parts = new String[]{"1S01", "1S01", "1S01", "1S01", "1S01", "1S02", "1S02", "1S02", "1H01", "1H01", "1S02", "1S01", "1S01", "1H01", "1H01", "1H01", "1S02", "1S02", "1M02", "1M02", "1M02"};
+
+        // Create Product Name Part Number map
+        Map<String, String> productNames = new TreeMap<>();
+        productNames.put("Blue Polo Shirt", "1S01");
+        productNames.put("Black Polo Shirt", "1S02");
+        productNames.put("Red Ball Cap", "1H01");
+        productNames.put("Duke Mug   ", "1M02");
+
+        // Create Product Counter Object and process data
+        int countProductCounter = productNames.size();
+    }
+
+    public ProductCounter(Map productNames) {
+        // Your code here
+    }
+
+    public void processList(String[] list) {
+        // your code here
+    }
+
+    public void printReport() {
+        // Your code here
+    }
+}
